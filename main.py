d4260497418c4906f58767e4483f774847b616b4
0Saitama0 <83511666+0Saitama0@users.noreply.github.com>
2021-05-02 00:49:09 -0400

    Update main.py
    Added some images

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import webbrowser
 from tkinter import *
+from tkinter.ttk import *
 count=0
-
-
 
 # Create the scraper
 root = Tk()
+
 def scrapper(url):
     value =" text too be scrapped"
     print("From: "+ url + " Scrapped information: "+ value)
@@ -14,8 +14,8 @@ def login():
         element.destroy()
     loginLabel = Label(root, text="Please login to d2l")
     loginLabel.grid(row=6, column=6)
-    # launching website to login
-    #webbrowser.open('https://cas.ryerson.ca/login?service=https://my.ryerson.ca/uPortal/Login%3FrefUrl%3D%2FuPortal%2Ff%2Fmyryerson%2Fnormal%2Frender.uP', new=0)
+    ##launching website to login
+    webbrowser.open('https://cas.ryerson.ca/login?service=https://my.ryerson.ca/uPortal/Login%3FrefUrl%3D%2FuPortal%2Ff%2Fmyryerson%2Fnormal%2Frender.uP', new=0)
 
 def pasteLinks():
     global pasteInput
@@ -32,21 +32,49 @@ def SubmitPressed():
     LinkLabel.grid(row=6,column=6)
     scrapper(url)
 
-MenuLabel = Label(root, text="Menu")
-MenuLabel.grid(row=0, column=0)
+root.geometry("1000x600")
 
+style1=Style()
+style1.configure('TButton', font=('calibri', 19, 'bold'),
+                foreground = 'black')
 
-D2LLoginButton = Button(root, text="Login to D2L", command = login)
-PasteLinksButton = Button(root, text="Paste Zoom Links",command = pasteLinks)
-UploadFilesButton = Button(root, text="Upload Files")
-SearchEveryThingButton = Button(root, text='Search everything')
+MenuLabel = Label(root, text="Topic Finder",style='TButton')
+MenuLabel.grid(row=0, column=1)
+img = PhotoImage(file = r"bars.png")
+Label(root, image = img).grid(row = 0, column = 0,
+       columnspan = 1, rowspan = 1, padx = 5, pady = 5)
+img_f = PhotoImage(file = r"search.png")
+Label(root, image = img_f).grid(row = 0, column = 2,
+       columnspan = 1, rowspan = 1, padx = 5, pady = 5)
 
+style=Style()
+style.configure('W.TButton', font=('calibri', 16, 'bold'),
+                bg='orange',foreground = 'black')
 
-D2LLoginButton.grid(row=3, column=-0,)
-PasteLinksButton.grid(row=4,column=0)
-UploadFilesButton.grid(row=5,column=0)
-SearchEveryThingButton.grid(row=6,column=0)
+D2LLoginButton = Button(root, text="Login to D2L",style='W.TButton',command = login)
+PasteLinksButton = Button(root, text="Paste Zoom Links",style='W.TButton',command = pasteLinks)
+UploadFilesButton = Button(root, text="Upload Files",style='W.TButton')
+SearchEveryThingButton = Button(root, text='Search everything',style='W.TButton')
 
+D2LLoginButton.grid(row=3, column=1,)
+img1 = PhotoImage(file = r"brightspace.png")
+#img1 = img.subsample(2, 2)
+Label(root, image = img1).grid(row = 3, column = 0,
+       columnspan = 1, rowspan = 1, padx = 5, pady = 5)
 
+PasteLinksButton.grid(row=4,column=1)
+img2 = PhotoImage(file = r"link.png")
+Label(root, image = img2).grid(row = 4, column = 0,
+       columnspan = 1, rowspan = 1, padx = 5, pady = 5)
+
+UploadFilesButton.grid(row=5,column=1)
+img3 = PhotoImage(file = r"upload.png")
+Label(root, image = img3).grid(row = 5, column = 0,
+       columnspan = 1, rowspan = 1, padx = 5, pady = 5)
+
+SearchEveryThingButton.grid(row=6,column=1)
+img4 = PhotoImage(file = r"search.png")
+Label(root, image = img4).grid(row = 6, column = 0,
+       columnspan = 1, rowspan = 1, padx = 5, pady = 5)
 
 root.mainloop()
